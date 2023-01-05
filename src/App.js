cdea206708163c354b67bf855c357532cad7dab9
Dahyeonni <youlove7r@gmail.com>
2023-01-05 18:50:23 +0900

    feat: todo props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
-import { TodoList } from './components/TodoList';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트 공부하기',
+      checked: true,
+    },
+    {
+      id: 1,
+      text: '컴포넌트 공부하기',
+      checked: true,
+    },
+    {
+      id: 1,
+      text: '일정 관리 앱 공부하기',
+      checked: false,
+    },
+  ]);
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 }
