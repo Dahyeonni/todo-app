8d542ec032bb8ba23bdade8f2472ef3f654b8060
Dahyeonni <youlove7r@gmail.com>
2023-01-05 18:23:48 +0900

    todolist templates

---
+++ src/components/TodoList.js
@@ -0,0 +1,11 @@
+import TodoListItem from './TodoListItem';
+
+export const TodoList = () => {
+  return (
+    <div className="TodoList">
+      <TodoListItem />
+      <TodoListItem />
+      <TodoListItem />
+    </div>
+  );
+};
